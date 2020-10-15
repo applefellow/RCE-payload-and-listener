61edeb78c74a81207d11c6e2d15b2f82f640d9f2
applefellow <53038964+applefellow@users.noreply.github.com>
2020-10-15 16:52:38 +0530

    Update af@server.py

--- af@server.py
+++ af@server.py
@@ -60,80 +60,24 @@ for word1 in range(130000):
         print ("      ",final1[0:1],'               ',final1[17:18],'               ',final2[0:1],'               ',final2[17:18],'               ',final3[0:1],'               ',final3[17:18],'               ',final4[0:1],'               ',final4[17:18],"  ")
     elif count == 130000:
         os.system('clear')
-
-        
-            # print(ProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50))
-            # print("Server Loading...",i,"%",end='\r')
-
-    #     trace = final1[0:6],final2[0:7],final2[7:15],final2[15:23],final2[23:31],final3[0:7],final3[7:15],final3[15:23],final3[23:31],final4[0:7],final4[7:15],final4[15:23],final4[23:27]
-    #     trace=str(trace)
-    #     table=trace.maketrans(",'()",4*" ")
-    #     print (trace.translate(table),end="\r")
-
-    # elif count >= 10000:
-    #     trace = final2[0:6],final2[6:14],final2[14:22],final2[22:30],final3[0:6],final3[6:14],final3[14:22],final3[22:30],final4[0:6],final4[6:14],final4[14:22],final4[22:30]
-    #     trace=str(trace)
-    #     table=trace.maketrans(",'()",4*" ")
-    #     print (trace.translate(table),end="\r")
-    
-
-    # print(trace,end="\r")
-    
-    # time.sleep(0.1)
-# for x in full:
-    
-#     a=x[0:8]+' ',x[8:16]+' ',x[16:24]+' ',x[24:32]+' '
-#     a=str(a)
-#     a=a.strip('(')
-#     a=a.strip(')')
-#     a=a.strip(',')
-#     a=a.strip("'")
-#     print('\n',a,end='\r',) 
-    # time.sleep(0.1)
 time.sleep(1)
-# os.system('clear')
-filename='server1'
+filename='server'
 file=open(filename,"r")
 for line in file:
     a=line.strip('\n')
     print(a)
     time.sleep(0.1)
-# os.system('cat /home/gangan/Desktop/server1')
 print("\n")
-
-# custom_fig = Figlet(font='poison')
-# print(custom_fig.renderText('3kala SERVER...'))
-# next create a socket object 
 s = socket.socket()        
 print("Socket successfully created")
-
-# # reserve a port on your computer in our 
-# # case it is 12345 but it can be anything 
 port = random.randint(1,65535)
-
-# # Next bind to the port 
-# # we have not typed any ip in the ip field 
-# # instead we have inputted an empty string 
-# # this makes the server listen to requests 
-# # coming from other computers on the network 
 s.bind(('', port))         
 print("socket binded to %s" %(port)) 
-
-# put the socket into listening mode 
 s.listen(5)    
 print("socket is listening...")            
-
-# a forever loop until we interrupt it or 
-# an error occurs 
 while True: 
-
-# Establish connection with client. 
   c, addr = s.accept()     
-  print('Got connection from', addr) 
-
-# send a thank you message to the client. 
+  print('Got connection from', addr)  
 c.send('Thank you for connecting') 
-
-# Close the connection with the client 
 c.close() 
                   
